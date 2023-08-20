7764debc8dac6b149e98fb9afa080cff20d120bd
Marco Marzetti <marco@lamehost.it>
2023-08-20 17:34:03 +0200

    Update aggregate-prefixes.rb
    Removed redundant version info

--- Formula/aggregate-prefixes.rb
+++ Formula/aggregate-prefixes.rb
@@ -4,7 +4,6 @@ class AggregatePrefixes < Formula
   desc "Fast IPv4 and IPv6 prefix aggregator written in Python"
   homepage "https://github.com/lamehost/aggregate-prefixes/"
   url "https://github.com/lamehost/aggregate-prefixes/archive/refs/tags/v0.7.1.tar.gz"
-  version "0.7.1"
   sha256 "23442c4f393ea855ed874c8c66f053cb03e21f85f2f33e49b1fab13c05a41924"
   license "MIT"
 
